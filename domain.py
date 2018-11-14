4b7761d196802222c788bd8a98d746837c1e033e
Jack Davies <jgdav6@student.monash.edu>
2018-11-14 17:01:11 +1100

    implemented token request

--- domain.py
+++ domain.py
@@ -1,9 +1,34 @@
 import yaml
+import json
 import requests
 
 
 # Constants
-LOCATIONS = 'https://api.domain.com.au/v1/listings/locations'
+SCOPE = 'api_listings_read'  # requested API
+GET_TOKEN = 'https://auth.domain.com.au/v1/connect/token'
+GET_LOCATIONS = 'https://api.domain.com.au/v1/listings/locations'
+TOKEN_FILE = 'token.json'
+
+
+# Token functions
+def get_token(client_id, client_secret, scope):
+    data = {'grant_type':'client_credentials', 'scope':scope}
+    request = requests.post(GET_TOKEN, data=data, auth=(client_id, client_secret))
+    token = request.json()
+    print(token)
+    return token
+
+def save_token(token):
+    with open(TOKEN_FILE, 'w+') as f:
+        json.dump(token, f)
+
+def load_token():
+    with open(TOKEN_FILE) as f:
+        return json.load(f)
+
+# To get and save a new token, uncomment below
+# token = get_token(CLIENT_ID, CLIENT_SECRET, SCOPE)
+# save_token(token)
 
 
 # Setup
@@ -14,19 +39,17 @@ CLIENT_SECRET = config['CLIENT_SECRET']
 with open('preferences.yml') as f:
     preferences = yaml.safe_load(f)
 print(preferences)
-
-
-# Get Token
-def get_token(client_id, client_secret):
-    pass
-print(get_token(CLIENT_ID, CLIENT_SECRET))
+try:
+    token = load_token()
+except FileNotFoundError:
+    print("token not found")
 
 
 # Test
 def get_locations(search_terms):
     locations = []
     for search_term in search_terms:
-        request = requests.get(LOCATIONS, params={'terms':search_term})
+        request = requests.get(GET_LOCATIONS, params={'terms':search_term})
         locations.append(request.json())
     return locations
 print(get_locations(['2000']))
